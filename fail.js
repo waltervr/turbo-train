286e6f9d00c704639fbb750bcb7c81f6701ef78e
Walter Vargas <wvargas@extraspace.com>
2023-06-20 10:36:29 -0600

    update

--- fail.js
+++ fail.js
@@ -2,11 +2,13 @@ import http from 'k6/http';
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 import { setFailure } from './logg.js';
 
+const errorCount = __ENV.errorCount ? __ENV.errorCount : 1;
+
 export const options = {
   thresholds: {
     "ErrorCounter": [{
       abortOnFail: false,
-      threshold: `count<${__ENV.errorCount}`
+      threshold: `count<${errorCount}`
     }]
   }
 }
