f641a203ecc5af477d62a85e02561cdc02bd70d5
Walter Vargas <wvargas@extraspace.com>
2023-06-20 08:18:23 -0600

    initial commit

---
+++ fail.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
+export const options = {
+  thresholds: {
+    ErrorCounter: [{
+      abortOnFail: false,
+      threshold: 'count<1'
+    }],
+    http_req_duration: ['p(95)<100']
+  }
+}
+
+export default function () {
+  const res = http.get('https://k6.io');
+  console.log(res.timings.duration)
+}
+
+export function handleSummary(data) {
+  console.log(data.metrics["ErrorCounter"]);
+
+  return {
+    stdout: textSummary(data, {
+      enableColors: true,
+      indent: " ",
+    }),
+  };
+}
