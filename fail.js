374981d3835975f7db40ae5d2b72e265bc69aee7
Walter Vargas <wvargas@extraspace.com>
2023-06-20 08:22:34 -0600

    sec commit

--- fail.js
+++ fail.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+import { setFailure } from './logg.js';
 
 export const options = {
   thresholds: {
