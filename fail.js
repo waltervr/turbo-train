cd7b6f3b865790a3dc1e0ab535bc98613a6d40ba
Walter Vargas <wvargas@extraspace.com>
2023-06-20 11:24:08 -0600

    update

--- fail.js
+++ fail.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js";
 import { setFailure } from './logg.js';
 
 const errorCount = __ENV.errorCount ? __ENV.errorCount : 1;
@@ -23,6 +24,8 @@ export function handleSummary(data) {
   console.log(data.metrics["ErrorCounter"]);
 
   return {
+    "apitest-report.html": htmlReport(data),
+    "apitest-summary.json": JSON.stringify(data),
     stdout: textSummary(data, {
       enableColors: false,
       indent: " ",
